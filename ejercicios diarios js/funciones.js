29e81bef13eb40e3b679478690a3ff754ec48367
Luz Angela Parra <136365904+Ngel07@users.noreply.github.com>
2024-09-02 07:02:07 +0200

    Add files via upload

---
+++ ejercicios diarios js/funciones.js
@@ -0,0 +1,23 @@
+// funcion declarada
+
+function funcionDeclarada(){
+    console.log("uno")
+    console.log("dos")
+    console.log("tres")
+}
+
+//invocacion de la funcion
+funcionDeclarada()
+funcionDeclarada()
+
+
+function queDevuelveValor (){
+    console.log("cuatro")
+    console.log("cinco")
+    console.log("seis")
+    return "retorna una cadena de texto"
+}
+
+let retornoDeLaFuncion = valorQueRetornaLaFuncion ();
+
+console.log(valorQueRetornaLaFuncion)
